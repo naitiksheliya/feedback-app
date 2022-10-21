18169f898c3dd506a90cc9e0a6ab92a1bdc5c355
unknown <naitiksheliya7603@gmail.com>
2022-10-21 10:10:19 +0530

    pre learing befor starting project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
+const title = "blog post";
+const body = "this is my blog post";
+const comments = [
+  { id: 1, text: "comment one" },
+  { id: 2, text: "comment two" },
+  { id: 3, text: "comment three" },
+];
+
+const showComments = true;
+const loding = false;
 function App() {
+  // return React.createElement('div',
+  // {className:'container'},
+  //     React.createElement('h1',{},'My app'))
+  // if(!loding){
+    const comments_box=(<div className="comments">
+    <h3> comments ({comments.length})</h3>
+    <ul>
+      {comments.map((comments, index) => (
+        <li key={index}>{comments.text}</li>
+      ))}
+    </ul>
+  </div>)
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>{title.toUpperCase()}</h1>
+      <p>{body}</p>
+
+      {showComments && comments_box}
     </div>
   );
 }
+
+// return(
+// <div className="container">
+//      <h1>{title.toUpperCase()}</h1>
+//         <p>{body}</p>
+
+//     <div className="comments">
+//         <h3> comments ({comments.length})</h3>
+//         <ul>{
+//                 comments.map((comments,index)=>(
+//                     <li key={index}>{comments.text}</li>
+//                 ))
+//             }
+//         </ul>
+//     </div>
+//  </div>
+// )}
+
+// }
 
 export default App;
